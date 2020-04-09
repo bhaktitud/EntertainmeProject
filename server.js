41cba64e8042f096037aec40cc6e9494098142d5
bhaktitud <myforework@live.com>
2020-04-09 15:26:59 +0700

    Update and Delete

--- server.js
+++ server.js
@@ -18,10 +18,18 @@ MongoClient.connect(connectionString, {
         const db = client.db('entertainme')
         const collection = db.collection('Movies')
 
+        //READ
         app.get('/', (req, res) => {
-            res.sendFile(__dirname + '/index.html')
+            db.collection('Movies').find({}).toArray()
+                .then((result) => {
+                    console.log(result)
+                    res.sendFile(__dirname + '/index.html')
+                }).catch((err) => {
+                    console.log(err)
+                });
         })
 
+        //CREATE
         app.post('/movie', (req, res) => {
             console.log(req.body)
             collection.insertOne(req.body)
@@ -33,11 +41,22 @@ MongoClient.connect(connectionString, {
                 });
         })
 
-        collection.find({}).toArray((err, result) => {
-            if(err) console.log(err)
+        //UPDATE
+        collection.update({_id: ObjectId('5e8ed8d2f5cc857136e7fae3')}, {$set: {title: 'ternyata aku'}})
+            .then((result) => {
+                console.log('data has been updated')
+            }).catch((err) => {
+                console.log(err)
+            });
 
-            console.log(result)
-        })
+        //DELETE
+        collection.remove({_id: ObjectId('5e8ed8d2f5cc857136e7fae3')})
+            .then((result) => {
+                console.log('Movie deleted')
+            }).catch((err) => {
+                console.log(err)
+            });
+
 
         app.listen(PORT, () => {
             console.log(`Mongo listening to the server : ${PORT}`)
