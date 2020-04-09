bb3d906cf8a6491b6b90f6c755267a9aacb4ec9c
bhaktitud <myforework@live.com>
2020-04-09 15:13:13 +0700

    Create and Read

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient
+const ObjectId = require('mongodb').ObjectId
 const app = express()
 const routes = require('./routes')
 const PORT = 3000
@@ -7,38 +8,40 @@ const connectionString = 'mongodb://127.0.0.1:27017/'
 
 
 app.use(express.urlencoded({ extended: false }))
-app.use(routes)
 
 MongoClient.connect(connectionString, {
     useNewUrlParser:true,
     useUnifiedTopology: true
-}, (err, client) => {
-    if(err){
+})
+    .then((client) => {
+        console.log('Connected to database')
+        const db = client.db('entertainme')
+        const collection = db.collection('Movies')
+
+        app.get('/', (req, res) => {
+            res.sendFile(__dirname + '/index.html')
+        })
+
+        app.post('/movie', (req, res) => {
+            console.log(req.body)
+            collection.insertOne(req.body)
+                .then((result) => {
+                    console.log(result)
+                    res.redirect('/')
+                }).catch((err) => {
+                    console.log(err)
+                });
+        })
+
+        collection.find({}).toArray((err, result) => {
+            if(err) console.log(err)
+
+            console.log(result)
+        })
+
+        app.listen(PORT, () => {
+            console.log(`Mongo listening to the server : ${PORT}`)
+        })
+    }).catch((err) => {
         console.log(err)
-        throw err
-    }
-
-    const db = client.db('entertainme')
-    const collection = db.collection('Movies')
-    collection.insert([
-        {
-            title: "Rambo V",
-            overview: "Petualangan Rambo",
-            poster_path: "http://www.google.com/rambo",
-            popularity: 4,
-            tags: [
-                "action",
-                "Drama"
-            ]
-
-        }
-    ], (err, result) => {
-        if(err) throw err
-
-        console.log('insert new data')
-    })
-})
-
-app.listen(PORT, () => {
-    console.log(`listen to port: ${PORT}`)
-})
+    });
